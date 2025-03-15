7042f548ff6fffe290d3178961ed4d7a7efe1362
Iris <flyima.tfd@laposte.net>
2025-03-15 15:07:09 -0400

    Upgraded In game menu

--- Assets/Scripts/UI/Menu/SecondaryButtonsActivation.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class SecondaryButtonsActivation : MonoBehaviour
-{
-    public static void SetActiveObject(GameObject gameObject)
-    {
-        gameObject.SetActive(!gameObject.activeSelf);
-    }
-}
