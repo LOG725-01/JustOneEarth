8521568ab6e481b8ddb4a725b152c34cdb2db1b8
Iris <flyima.tfd@laposte.net>
2025-04-05 17:38:48 -0400

    AI wait 2 sec, fix score observer

--- Assets/Scripts/UI/ScoreObserver.cs
+++ Assets/Scripts/UI/ScoreObserver.cs
@@ -7,6 +7,7 @@ public class ScoreObserver : Observer
     [SerializeField] private Slider slider;
 
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private PlayerType playerType;
 
     private void Awake()
     {
@@ -23,6 +24,7 @@ public class ScoreObserver : Observer
     {
         if (subject.TryGetComponent<Player>(out var player))
         {
+            if (player.PlayerType != playerType) return;
             if (slider.maxValue < player.Points) { Display((int)slider.maxValue); }
             else if (player.Points <= 0) { Display(0); }
             else { Display(player.Points); }
