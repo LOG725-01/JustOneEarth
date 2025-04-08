e83d211eb697e1cf59e1481fc9d444d9953c6e0d
Dragost37 <dragostgames@gmail.com>
2025-04-08 19:44:18 -0400

    Résolution merge conflict

--- Assets/Scripts/GameLogic/Cards/GetOnePointCard.cs
+++ Assets/Scripts/GameLogic/Cards/GetOnePointCard.cs
@@ -5,7 +5,6 @@ public class GetOnePointCard : CardData
         cardName = "PointBurst";
         description = "Get +1 score point ! Cost -1 of each ! ";
         cost.Add(RessourceTypes.Trees, 1);
-<<<<<<< HEAD
         cost.Add(RessourceTypes.Water, 1);
         cost.Add(RessourceTypes.Oil, 1);
         cost.Add(RessourceTypes.Minerals, 1);
@@ -15,8 +14,5 @@ public class GetOnePointCard : CardData
 
         GainPointEffect GetOnePointEffect = new GainPointEffect();
         effectList.Add(GetOnePointEffect);
-=======
-        effectList.Add(new GainPointEffect());
->>>>>>> 78f2d8a4ae53b486453951360391a39ac863c965
     }
 }
