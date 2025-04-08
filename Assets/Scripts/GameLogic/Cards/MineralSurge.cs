297c73a69aacd061aba95120d91b6ad0adae0774
Dragost37 <dragostgames@gmail.com>
2025-04-07 21:10:12 -0400

    Correction erreur suite au renomamge de l'effet

--- Assets/Scripts/GameLogic/Cards/MineralSurge.cs
+++ Assets/Scripts/GameLogic/Cards/MineralSurge.cs
@@ -6,7 +6,7 @@ public class MineralSurge : CardData
         description = "Exemple : Gagne 5 ressources de Minerais en dépensent 5 bois.";
         cost.Add(RessourceTypes.Trees, 5);
 
-        GainRessourceOfTileType gainRessourceOfTileType = new GainRessourceOfTileType(RessourceTypes.Minerals, 5);
+        GainRessourceOfType gainRessourceOfTileType = new GainRessourceOfType(RessourceTypes.Minerals, 5);
         effectList.Add(gainRessourceOfTileType);
     }
 }
