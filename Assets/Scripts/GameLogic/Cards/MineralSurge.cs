064137f60c707acc5d2f96d33064e7290617c2a2
Dragost37 <dragostgames@gmail.com>
2025-04-07 21:08:23 -0400

    Ajout debug false + renommage de l'effet et de la carte

--- Assets/Scripts/GameLogic/Cards/MineralSurge.cs
+++ Assets/Scripts/GameLogic/Cards/MineralSurge.cs
@@ -1,9 +1,9 @@
-public class RessourcesSurge : CardData
+public class MineralSurge : CardData
 {
     private void OnEnable()
     {
         cardName = "Mineral Surge";
-        description = "Exemple : Gagne 5 ressources en fonction de la tuile sélectioner en dépensent 5 bois.";
+        description = "Exemple : Gagne 5 ressources de Minerais en dépensent 5 bois.";
         cost.Add(RessourceTypes.Trees, 5);
 
         GainRessourceOfTileType gainRessourceOfTileType = new GainRessourceOfTileType(RessourceTypes.Minerals, 5);
